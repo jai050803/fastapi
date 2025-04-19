1a6c50e5f430e4af2012039e8cd0d8b7f748f378
jai <76526688+jai050803@users.noreply.github.com>
2025-04-19 17:21:30 +0530

    Delete request.py

--- request.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-response = requests.delete("http://localhost:8000/inventory/1")
-print(response.json())
-
-# Delete by name
-response = requests.delete("http://localhost:8000/inventory/by-name/Aspirin")
-print(response.json())
